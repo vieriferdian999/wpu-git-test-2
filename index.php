975a2022f8cba5401abc3e9c6f3f0d9dec7b4b06
Vieri Ferdian <vieriferdian9@gmail.com>
2023-04-12 01:52:50 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Helllo World</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis esse perspiciatis labore sequi voluptate ipsam? Accusamus assumenda minus incidunt, impedit beatae quibusdam aspernatur corporis nihil quod maiores, nesciunt veniam quia?</p>
+    <ul>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nisi voluptas nostrum? Ratione minus magni quidem natus. Nulla, corporis quia sapiente sit hic voluptatem ullam facere eos! Cupiditate, hic! Molestiae.</li>
+        <li>2314314</li>
+    </ul>
 </body>
 </html>
